7bda48093022888572966184a2ab4cd8be9efa9f
hardik <hardik12498@gmail.com>
2018-10-21 23:32:35 +0530

    Added place picker on fab button in main activity

---
+++ androidApp/app/src/main/java/com/codefundo/saveme/models/Camp.java
@@ -0,0 +1,10 @@
+package com.codefundo.saveme.models;
+
+public class Camp {
+
+    // Create a Easy table with name same as class.
+    public String address, postalCode;//,phonenumber,email,status,memberType;
+    public String id; // YOu need a ID for this thing to work
+
+    public Double currentLat, currentLong;
+}
