3dbf6739109ec42e547f40e2f7fd66c1d1d3d86d
hardik <hardik12498@gmail.com>
2018-10-26 04:19:33 +0530

    Updating donation fragment

--- androidApp/app/src/main/java/com/codefundo/saveme/donations/DonationFragment.java
+++ androidApp/app/src/main/java/com/codefundo/saveme/donations/DonationFragment.java
@@ -1,14 +1,29 @@
 package com.codefundo.saveme.donations;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.codefundo.saveme.KillableRunnable;
 import com.codefundo.saveme.R;
+import com.codefundo.saveme.SaveMe;
+import com.codefundo.saveme.models.Donation;
 import com.google.android.material.card.MaterialCardView;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceList;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+
+import org.jetbrains.annotations.NotNull;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,7 +34,8 @@ import androidx.fragment.app.Fragment;
 public class DonationFragment extends Fragment implements View.OnClickListener {
 
     private MaterialCardView selfCard;
-
+    private TextView tv_no_of_donors, tv_total_amount;
+    private KillableRunnable killableRunnable = null;
     public DonationFragment() {
         // Required empty public constructor
     }
@@ -41,6 +57,10 @@ public class DonationFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         selfCard = (MaterialCardView) inflater.inflate(R.layout.fragment_donations,
                 container, false);
+        tv_no_of_donors = selfCard.findViewById(R.id.tv_no_of_donors);
+        tv_total_amount = selfCard.findViewById(R.id.tv_total_donation);
+
+        update();
         return selfCard;
     }
 
@@ -48,6 +68,7 @@ public class DonationFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onResume() {
         super.onResume();
+        update();
         selfCard.setOnClickListener(this);
     }
 
@@ -63,5 +84,36 @@ public class DonationFragment extends Fragment implements View.OnClickListener {
         startActivity(donationActivityIntent);
     }
 
+    private void update() {
 
+        final Context context = getContext();
+        Handler handler = new Handler();
+        killableRunnable = new KillableRunnable() {
+            @Override
+            public void doWork() {
+                MobileServiceClient mClient = SaveMe.getAzureClient(context);
+                MobileServiceTable<Donation> table = mClient.getTable(Donation.class);
+                ListenableFuture<MobileServiceList<Donation>> listListenableFuture = table.where().execute();
+                Futures.addCallback(listListenableFuture, new FutureCallback<MobileServiceList<Donation>>() {
+                    @Override
+                    public void onSuccess(MobileServiceList<Donation> result) {
+
+                        tv_no_of_donors.setText(String.valueOf(result.size()));
+                        int total_donation = 0;
+                        for (Donation donation : result)
+                            total_donation += donation.getAmount();
+                        tv_total_amount.setText(String.valueOf(total_donation));
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Throwable t) {
+                        Log.e("Report Activity:Error: ", t.getMessage());
+                    }
+                });
+
+                handler.postDelayed(killableRunnable, 10000);
+            }
+        };
+        handler.post(killableRunnable);
+    }
 }
