1829113e55b014d1341bdb715f422584f26d660b
aayushsingla <f20170815@goa.bits-pilani.ac.in>
2018-10-26 11:53:32 +0530

    final changes and bug fixes

--- androidApp/app/src/main/java/com/codefundo/saveme/admin/AdminActivity.java
+++ androidApp/app/src/main/java/com/codefundo/saveme/admin/AdminActivity.java
@@ -3,11 +3,14 @@ package com.codefundo.saveme.admin;
 import android.os.Bundle;
 
 import com.codefundo.saveme.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import static com.codefundo.saveme.maps.MapActivity.launchMapActivity;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -19,6 +22,10 @@ public class AdminActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         toolbar.setNavigationOnClickListener(view -> onBackPressed());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> launchMapActivity(this, "user"));
+
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(new AdminAdapter());
--- androidApp/app/src/main/java/com/codefundo/saveme/rescueteam/RescueActivity.java
+++ androidApp/app/src/main/java/com/codefundo/saveme/rescueteam/RescueActivity.java
@@ -1,15 +1,24 @@
 package com.codefundo.saveme.rescueteam;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Switch;
 
 import com.codefundo.saveme.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import static com.codefundo.saveme.maps.MapActivity.launchMapActivity;
+
 public class RescueActivity extends AppCompatActivity {
+    public static final String VOLUNTEER_LOCATION = "isVolunteerOnline";
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +26,26 @@ public class RescueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rescue);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         toolbar.setNavigationOnClickListener(view -> onBackPressed());
+
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(new RescueTeamAdapter());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> launchMapActivity(this, "user"));
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(RescueActivity.this);
+        boolean isChecked = sharedPreferences.getBoolean(VOLUNTEER_LOCATION, true);
+        Switch swich = findViewById(R.id.switch_volunteer_online);
+        swich.setChecked(isChecked);
+        swich.setOnCheckedChangeListener((buttonView, isChecked1) -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(VOLUNTEER_LOCATION, isChecked1);
+            Log.e(VOLUNTEER_LOCATION, isChecked1 + "");
+            editor.apply();
+        });
     }
 }
